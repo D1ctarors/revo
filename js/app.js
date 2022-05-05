f79bbb1ccbfae60b764979fea7459df6f917f233
D1ctators <102908509+D1ctarors@users.noreply.github.com>
2022-05-05 15:53:34 +0300

    Delete js directory

--- js/app.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$('.body-section-site-1__burger').click(function(event){
-		$('.body-section-site-1__burger, .header').toggleClass('active');
-		$('body').toggleClass('lock');
-	})
-});
